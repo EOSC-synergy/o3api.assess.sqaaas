938065dcf1ccf6cec9d9a7ad8095849fcaa8617d
orviz <orviz@ifca.unican.es>
2022-07-20 16:36:25 +0200

    Add JePL file structure

--- .sqa/script.hazy-cerise-monster.sh
+++ .sqa/script.hazy-cerise-monster.sh
@@ -1,4 +1,4 @@
 (
-cd github.com/EOSC-synergy/o3api &&
+cd git.scc.kit.edu/synergy.o3as/o3api &&
     git rev-parse --is-inside-work-tree
 )
--- .sqa/script.wimpy-mauve-kakapo.sh
+++ .sqa/script.wimpy-mauve-kakapo.sh
@@ -1,4 +1,4 @@
 (
-cd github.com/EOSC-synergy/o3api &&
+cd git.scc.kit.edu/synergy.o3as/o3api &&
     bandit -f json --quiet  --confidence-level high --severity-level high --recursive .
 )
