a4ae5784fa0b70ceab9a67b7aaa10b477edc98df
orviz <orviz@ifca.unican.es>
2022-10-05 10:04:26 +0200

    Add JePL file structure

--- .sqa/script.jumpy-chartreuse-owl.sh
+++
@@ -1,4 +0,0 @@
-(
-cd git.scc.kit.edu/synergy.o3as/o3api &&
-    fuji.py -OID http://hdl.handle.net/10261/157765 --tool_endpoint http://localhost:1071/fuji/api/v1/evaluate -T oai_pmh -E http://digital.csic.es/dspace-oai/request
-)
