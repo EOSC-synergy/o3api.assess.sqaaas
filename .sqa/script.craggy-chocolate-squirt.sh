3b04db439dd712925f2c06d451ba09168aff1892
orviz <orviz@ifca.unican.es>
2022-12-15 18:09:33 +0100

    Add JePL file structure

--- .sqa/script.craggy-chocolate-squirt.sh
+++ .sqa/script.craggy-chocolate-squirt.sh
@@ -1,4 +1,4 @@
 (
-cd git.scc.kit.edu/synergy.o3as/o3api &&
+cd github.com/EOSC-synergy/o3api &&
     licensee detect . --confidence 60 --json
 )
--- .sqa/script.silly-wisteria-wolverine.sh
+++ .sqa/script.silly-wisteria-wolverine.sh
@@ -1,4 +1,4 @@
 (
-cd git.scc.kit.edu/synergy.o3as/o3api &&
+cd github.com/EOSC-synergy/o3api &&
     bandit -f json --quiet  --confidence-level high --severity-level high --recursive .
 )
